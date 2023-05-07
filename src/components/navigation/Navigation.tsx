cc83b57abd865a6641dfe36df27abbd41ec1c6b2
Daniel Cruz <danielestupinan@bemaster.com>
2023-05-06 22:33:48 -0500

    feat(navigation): :lipstick:  add icons and styles

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -1,30 +1,48 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
+import { ContactIcon, KnowledgeIcon, ProfileIcon, WorkingExpIcon } from '../icons/nav-bar-icons/NavBarIcons.tsx'
 
 export function Navigation () {
+
+  function isActiveClass ({ isActive }: { isActive: boolean, isPending: boolean }): string {
+    return isActive ? 'active' : ''
+  }
+
   return (
-    <>
+    <section className="navigation-wrapper">
       <ul className="navigation">
-        <li>
-          <Link to={''} className="bubble-link">
+        <li className="bubble-link">
+          <div className="bubble-link__img">
+            <ProfileIcon/>
+          </div>
+          <NavLink to={''} className={isActiveClass} end>
             Perfil
-          </Link>
+          </NavLink>
         </li>
-        <li>
-          <Link to={'working-experience'} className="bubble-link">
-            Experiencia laboral
-          </Link>
+        <li className="bubble-link">
+          <div className="bubble-link__img">
+            <KnowledgeIcon/>
+          </div>
+          <NavLink to={'working-experience'} className={isActiveClass}>
+            Experiencia
+          </NavLink>
         </li>
-        <li>
-          <Link to={'knowledge'} className="bubble-link">
-            Conocimientos
-          </Link>
+        <li className="bubble-link">
+          <div className="bubble-link__img">
+            <WorkingExpIcon/>
+          </div>
+          <NavLink to={'knowledge'} className={isActiveClass}>
+            Saberes
+          </NavLink>
         </li>
-        <li>
-          <Link to={'contact'} className="bubble-link">
+        <li className="bubble-link">
+          <div className="bubble-link__img">
+            <ContactIcon/>
+          </div>
+          <NavLink to={'contact'} className={isActiveClass}>
             Contacto
-          </Link>
+          </NavLink>
         </li>
       </ul>
-    </>
+    </section>
   )
 }
